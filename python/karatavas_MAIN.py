8609a4208394eb333102e384413ba86505cde191
Edzass <93673275+Edzass@users.noreply.github.com>
2025-11-09 20:03:19 +0200

    1
    1

--- python/karatavas_MAIN.py
+++ python/karatavas_MAIN.py
@@ -1,22 +1,29 @@
 import random
+import os
 
 vardu_saraksts = ["ābols", "banāns", "ķirsis", "melone", "apelsīns", "zemene", "vīnoga"]
 atbilde = "j"
 
-dzivibu_skaits = 6
-meklejamais_vards = ""
-varda_stavoklis = []
-meklētie_burti = []
-karatavas_attels = []
+
 
 # Funkcija, kas attēlo karātavas skici atbilstoši atlikušo dzīvību skaitam
 def attels():
     global dzivibu_skaits
-    if dzivibu_skaits == 5:
+    if dzivibu_skaits == 6:
         print("""
         +---+
         |   |
             |
+            |
+            |
+            |
+        =========
+        """)
+    elif dzivibu_skaits == 5:
+        print("""
+        +---+
+        |   |
+        O   |
             |
             |
             |
@@ -27,7 +34,7 @@ def attels():
         +---+
         |   |
         O   |
-            |
+        |   |
             |
             |
         =========
@@ -37,7 +44,7 @@ def attels():
         +---+
         |   |
         O   |
-        |   |
+       /|   |
             |
             |
         =========
@@ -47,7 +54,7 @@ def attels():
         +---+
         |   |
         O   |
-       /|   |
+       /|\  |
             |
             |
         =========
@@ -57,7 +64,7 @@ def attels():
         +---+
         |   |
         O   |
-       /|   |
+       /|\  |
        /    |
             |
         =========
@@ -74,18 +81,17 @@ def attels():
         """)
     return
 
-# Funkcija, kas atiestata spēles mainīgos, ja spēle tiek atkārtota
-def vertibas():
-    global dzivibu_skaits, varda_stavoklis, meklētie_burti, meklejamais_vards
-    dzivibu_skaits = 6
-    meklejamais_vards = ""
-    varda_stavoklis = []
-    meklētie_burti = []
-    return
+
 
 # Funkcija, kas uzskāk spēli, izvēloties nejaušu vārdu no saraksta un sagatavo spēles mainīgos
 def sakums():
-    global meklejamais_vards, varda_stavoklis, meklētie_burti, dzivibu_skaits
+    
+    global dzivibu_skaits, varda_stavoklis, meklētie_burti, meklejamais_vards
+    dzivibu_skaits = 7
+    meklejamais_vards = ""
+    varda_stavoklis = []
+    meklētie_burti = []
+    
     meklejamais_vards = vardu_saraksts[random.randint(0, len(vardu_saraksts)-1)]
     for x in meklejamais_vards:
         varda_stavoklis.append("_")
@@ -113,7 +119,7 @@ def spele():
                     break
                             
             elif ievade in meklētie_burti or ievade in varda_stavoklis:
-                print("Jau meklēji burutu:", ievade)
+                print("Jau meklēji burtu:", ievade)
             else:
                 dzivibu_skaits -=1
                 meklētie_burti.append(ievade)
@@ -122,12 +128,15 @@ def spele():
             print("Dzivibu skaits:", dzivibu_skaits)
             print("Nederīgie burti:", meklētie_burti)
             print("")
-
+    if dzivibu_skaits == 0:
+        print("Spēle ir noslēgusies! Meklējamais vārds bija:", meklejamais_vards.upper())
 
 # Programmas cilks, kas izpilda funkcijas, lai spēle funkcionētu un būtu iespēja to atkārtot
 while atbilde == "j":
     sakums()
     spele()
     atbilde = input("Vai vēlies atkārtot spēli? (j/n)")
-    vertibas()
+    # notīra ekrānu pēc spēles beigām
+    os.system('cls')
+    
 
